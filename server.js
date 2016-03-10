0112c8434774c005e489c24b7d44a0489ec10775
Alex Ewerlöf <hanifbbz@gmail.com>
2016-03-10 17:22:35 +0100

    added the auto compiler for ECMA 6

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
+var babel = require('babel-core');
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const options =  {
+  frontendRoot: 'www',
+  port: 3000
+}
+const BABEL_OPTIONS = {
+  presets: ['es2015'],
+  plugins: [],
+  sourceMaps: 'inline'
+};
+
+app.get('/*.es', function (req, res, next) {
+  var fileName = path.format({
+    dir: options.frontendRoot,
+    base : req.url
+  });
+  console.log(`Compiling file: ${fileName}`);
+  try {
+    babel.transformFile(fileName, BABEL_OPTIONS, (err, result) => {
+      if (err) {
+        res.status(404).send(err);
+      } else {
+        res.send(result.code);
+      }
+    });
+  } catch (e) {
+    res.status(404).send(`Cannot find ${fileName}`);
+  }
 });
 
-app.listen(3000);
+app.use(express.static(options.frontendRoot));
+
+app.listen(options.port, function () {
+  console.log(`Listenning on ${options.port}`);
+});
