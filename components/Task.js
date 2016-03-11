5952f86b92b83eac55898dcb3fa6998356523877
Gustaf Forsslund <gustaf.forsslund@oktavilla.se>
2016-03-11 20:07:48 +0100

    Fixes propagation problem

--- components/Task.js
+++ components/Task.js
@@ -2,9 +2,9 @@ import React, { PropTypes } from 'react'
 
 const Task = ({ onChange, pair, label }) => (
   <tr>
-    <td><input type='text' defaultValue={ pair[0] } onChange={ e => onChange('pair', e.target.value, 0) }/></td>
-    <td><input type='text' defaultValue={ label } onChange={ e => onChange('label', e.target.value) }/></td>
-    <td><input type='text' defaultValue={ pair[1] } onChange={ e => onChange('pair', e.target.value, 1) }/></td>
+    <td><input type='text' value={ pair[0] } onChange={ e => onChange('pair', e.target.value, 0) }/></td>
+    <td><input type='text' value={ label } onChange={ e => onChange('label', e.target.value) }/></td>
+    <td><input type='text' value={ pair[1] } onChange={ e => onChange('pair', e.target.value, 1) }/></td>
   </tr>
 )
 
--- reducers/index.js
+++ reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 import tasks from './tasks'
 
-const rotatingPairsApp = combineReducers({
-  tasks
-})
+const rotatingPairsApp = combineReducers({ tasks })
 
 export default rotatingPairsApp
