3bde457e0164ce880a2f466909620d096382f187
Gustaf Forsslund <gustaf.forsslund@oktavilla.se>
2016-03-11 20:07:36 +0100

    Removes state mutation (sorry)

--- reducers/tasks.js
+++ reducers/tasks.js
@@ -1,6 +1,9 @@
+import cloneDeep from 'lodash/fp/cloneDeep'
+
 const tasks = (state = [], action) => {
   switch (action.type) {
     case 'update-task':
+      state = cloneDeep(state) // Ok, so this is horrible - there must be another way
       let task = state.find(task => task.id === action.id)
       if (typeof action.propertyIndex !== "undefined") {
         task[action.property][action.propertyIndex] = action.value
@@ -12,6 +15,7 @@ const tasks = (state = [], action) => {
       return [...state, { id: action.id, label: '', pair: ['', ''] }]
     case 'rotate-pairs':
       if (state.length > 1) {
+        state = cloneDeep(state)
         let previous = state[action.direction === 1 ? state.length - 1 : 0].pair[action.pairMember]
         for (let i = 0, ln = state.length; i < ln; i++) {
           let index = action.direction === 1 ? i : ln - i - 1
